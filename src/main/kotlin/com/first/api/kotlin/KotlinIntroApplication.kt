31513b3edbcd084e5a7237bf9d91265c3cbb7138
zalacop <103320914+zalacop@users.noreply.github.com>
2025-09-08 20:27:35 +0200

    kotlin first api

---
+++ src/main/kotlin/com/first/api/kotlin/KotlinIntroApplication.kt
@@ -0,0 +1,11 @@
+package com.first.api.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinIntroApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinIntroApplication>(*args)
+}
---
+++ src/test/kotlin/com/first/api/kotlin/KotlinIntroApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.first.api.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinIntroApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
