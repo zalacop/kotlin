1df2ecbaeb15960d3ca98b06ee9130e9b877feda
zalacop <103320914+zalacop@users.noreply.github.com>
2025-09-19 11:35:06 +0200

    getting book by id

--- src/main/kotlin/com/first/api/book/BookController.kt
+++ src/main/kotlin/com/first/api/book/BookController.kt
@@ -1,6 +1,7 @@
 package com.first.api.book
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -11,6 +12,11 @@ class BookController {
         return listOf(BookResponse(id = 1, title = "Mistborn", author = "Brandon Sanderson"),
                       BookResponse(id = 2, title = "Well of Ascension", author = "Brandon Sanderson"),
                       BookResponse(id = 3, title = "Hero of Ages", author = "Brandon Sanderson"))
+    }
+
+    @GetMapping("/books/{id}")
+    fun getBookById(@PathVariable id: Int ): BookResponse {
+        return BookResponse(id = id, title = "Mistborn", author = "Brandon Sanderson")
     }
 }
 
